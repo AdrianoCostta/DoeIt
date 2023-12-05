9a90ebc1232f7c70ad0aa704f361af39dff11325
bernadigo <bernadigo@gmail.com>
2023-12-04 20:37:41 -0400

    Tela de doação criada, parte do criar do CRUD codificada. Parte da leitura de dados em andandamento.

---
+++ app/src/main/java/com/example/doeit/MyAdapter.java
@@ -0,0 +1,29 @@
+package com.example.doeit;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+}
+
+class MyViewHolder extends RecyclerView.ViewHolder{
+
+    ImageView recImage;
+    TextView recTile, recDesc, recStatus;
+    CardView recCard;
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recImage = itemView.findViewById(R.id.recImage);
+        recCard = itemView.findViewById(R.id.recCard);
+        recImage = itemView.findViewById(R.id.recImage);
+        recImage = itemView.findViewById(R.id.recImage);
+
+    }
+}
